b7bd3b73c58cff740d2f807577ea6c4e5f5cb6c9
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-08-27 22:29:50 -0300

    xota

--- main.cpp
+++ main.cpp
@@ -17,7 +17,11 @@ using namespace std;
 // Function to calculate f(x)
 float func(float x)
 {
-    return cbrt(x);
+    x = (-pow(x,2)/2);
+    x = exp(x);
+    float temp = 2 * 3.1415;
+    float aux = pow(temp, 0.5);
+    return x / aux;
 } 
 
 // Function for approximate integral
@@ -149,17 +153,134 @@ float simpson_(float ll, float ul, int n, int p, int manualWay)
 }
 
 
+float simpsons(float ll, float ul, int n, int p, int manualWay)
+{
+    // Calculating the value of h
+    float h = roundf(((ul - ll) / n) * p) / p;
 
+    // Array for storing value of x and f(x)
+    float x[10], fx[10], cy[10];
 
+    if(manualWay == 0) {
+        // Calculating values of x and f(x)
+        for (int i = 0; i <= n; i++)
+        {
+            x[i] = roundf((ll + i * h) * p) / p;
+            fx[i] = roundf((func(x[i])) * p) / p;
+        }
+    }
+    else if(manualWay == 1) {
+        // Reading points
+        for (int i = 0; i <= n; i++)
+        {
+            cout << "Ponto " << i << ": ";
+            cin >> x[i] >> fx[i];
+        }
+    }
 
+    float value;
+    float interval_size = roundf(((ul - ll) / n) * p) / p;
+    float sum = (roundf(func(ll) * p) / p) + (roundf(func(ul) * p) / p); 
 
+    // Calculates value till integral limit
 
+    for (int i = 1 ; i < n ; i++)
+    {
+        if (i % 3 == 0)
+            sum = sum + 2 * (roundf(func(ll + i * interval_size) * p) / p);
+        else
+            sum = sum + 3 * (roundf(func(ll + i * interval_size) * p) / p);
+    }
+    float res = 0;
+    res = (roundf((( 3 * interval_size / 8 ) * sum) * p) / p);
 
+    std::cout << "      ************************|RESULTADOS|************************" << "\n\n";
+    std::cout << "Tabela:" << std::endl;
+    int c = 0;
+    for(int i = 0; i < (n+1); i++){ 
+        if(i==0 || i == n){
+            c=1;
+        }else if(i%3==0){
+            c=2;
+        }else{
+            c=3;
+        }
+        std::cout << "i" << i << " = " << i;
+        std::cout << "           X" << i << " = " << x[i]; 
+        std::cout << "           Y" << i << " = " << fx[i];
+        std::cout << "           C" << i << " = " << c;
+        std::cout << "           Y" << i << "*"<< "C"<< i << " = " << fx[i]*c << "\n\n";
+    }
 
+    std::cout <<"O valor de h e: "<< h << "\n";
+    std::cout << "O valor da integracao e: " << res << "\n\n";
 
+    std::cout << "      ************************|RASCUNHO|************************" << "\n\n";
 
+    std::cout << "CALCULO DO H: " << "\n\n";
+    std::cout << "(" << ul <<"-" << ll <<")"<<"\n";
+    std::cout << "-----------------" <<"   =  " << h << "\n";
+    std::cout <<"    " << n <<"\n\n\n\n";
 
+    std::cout << "CALCULO DO RESULTADO FINAL(INTEGRACAO): " << "\n";
+    cout << "\n";
+    cout << "Formula: " << "\n";
+    cout << "3h/8" << "( ";
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0){
+            cout << "fx[i]" << " + ";
+        }
+        else if(i == n){
+            cout << "fx[i]";
+        }
+        else if(i % 3 == 0){
+            cout << "2*fx[i]";    
+        }
+        else{
+            cout << "3*fx[i]" << " + ";
+        }
+    }
+    cout << " )" << "\n\n";
+    
+    cout << h << " * 3/8" << "( ";
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0){
+            cout << fx[i] << " + ";
+        }
+        else if(i == n){
+            cout << fx[i];
+        }
+        else if(i % 3 == 0){
+            cout << "2*" << fx[i]<< " + "; 
+        }
+        else{
+            cout << "3 *" <<fx[i]<< " + ";
+        }
+    }
+    cout << " )" << "\n";
 
+    cout << h*3/8 << "( ";
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0){
+            cout << fx[i] << " + ";
+        }
+        else if(i == n){
+            cout << fx[i];
+        }
+        else if(i % 3 == 0){
+            cout << 2*fx[i]<< " + "; 
+        }
+        else{
+            cout << 3*fx[i] << " + ";
+        }
+    }
+    cout << " )" <<" = "<< res << "\n\n";
+
+    return res;
+}
 
 float trapezio(float ll, float ul, float n, int p,int manualWay)
 {
@@ -318,7 +439,7 @@ int main()
       trapezio(lower_limit, upper_limit, n, p,manualWay);
       break;
     case '3':
-    //   simpsons(lower_limit, upper_limit, n, p,manualWay);
+      simpsons(lower_limit, upper_limit, n, p,manualWay);
       break;
     default: std::cerr << "Invalid type\n";
   }
